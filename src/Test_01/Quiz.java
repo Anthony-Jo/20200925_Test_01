76d1bc214075ef25dc50437188d28b4d78bc6a43
skjo <zamake@naver.com>
2020-09-25 16:20:28 +0900

    2020-09-25_03

---
+++ src/Test_01/Quiz.java
@@ -0,0 +1,26 @@
+package Test_01;
+
+public class Quiz
+{
+	public static void main(String[] args) 
+	{
+		final String NAME = "Name";
+		final String AGE =  "Age";
+		final String HEIGHT = "Height";
+		final String IQ = "IQ";
+		final String LEVEL = "Level";
+		
+		final String NAME_VALUE = "Anthony_Jo";
+		int age_value = 20;						/* JAVA has no 'unsigned'	*/
+		double height_value = 173.3;
+		int iq_value = 120;
+		char level_value = 'B';
+		
+		System.out.println(NAME + ": " + NAME_VALUE);
+		System.out.println(AGE + ": " + age_value);
+		System.out.println(HEIGHT + ": " + height_value);
+		System.out.println(IQ + ": " + iq_value);
+		System.out.println(LEVEL + ": " + level_value);
+	}
+
+}
---
+++ src/Test_01/Test_03.java
@@ -0,0 +1,12 @@
+package Test_01;
+
+public class Test_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final String KOREA = "¥Î«—πŒ±π";
+		System.out.println("Korea: " + KOREA);
+
+	}
+
+}

424b4756097f685b786f26a2930449ac190fce39
skjo <zamake@naver.com>
2020-09-25 15:46:45 +0900

    2020-09-25_01

---
+++ src/Test_01/Test_01.java
@@ -0,0 +1,25 @@
+package Test_01;
+
+public class Test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 229;
+		double pi = 3.14;
+		double height = 178.8;
+		char name = 'J';
+		
+		num = num + 100;
+		name = 'A';
+		
+		System.out.println("Hello JAVA");
+		System.out.println("it is good day to die");
+		System.out.println("Your nunmber is " + num + ":)");
+		System.out.println("pi = " + pi + ":D");
+		System.out.println("My heigth is " + height + "cm ^_^ ");
+		System.out.println("your name is " + name + " OK?" );
+		/*	git hub test 03	*/
+	}
+
+}
---
+++ src/Test_01/Test_02.java
@@ -0,0 +1,12 @@
+package Test_01;
+
+public class Test_02 {
+	public static void main(String[] args) {
+		char ch = 'A';
+		
+		System.out.println("ch: " + ch );
+		System.out.println("(int)ch: " + (byte)ch);
+		/*	hello world	*/
+	}
+
+}

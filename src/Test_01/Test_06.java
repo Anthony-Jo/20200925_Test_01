4c52f6fc941b2ed7a512d8dfda3011c599906844
skjo <zamake@naver.com>
2020-10-07 09:26:24 +0900

    2020-10-07

--- src/Test_01/Test_06.java
+++ src/Test_01/Test_06.java
@@ -7,12 +7,11 @@ public class Test_06 {
 	public static void main(String[] args) throws IOException 
 	{ 
 		System.out.print("Input char: "); 
+		
 		int num =System.in.read(); 
+		
 		System.out.println("==== Out Put ===="); 
 		System.out.println("Inputed char :" + (char)num); 
 		System.out.println("Changed :" + (char)(num - 32));
 	}
-
-
-
 }
